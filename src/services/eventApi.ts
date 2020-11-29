f3bf041ce0f106b7ff2df1b465b83ed3f532a95d
bwapes <benwaples@gmail.com>
2020-11-28 17:26:12 -0800

    get all events from API

--- src/services/eventApi.ts
+++ src/services/eventApi.ts
@@ -4,3 +4,5 @@ import { get, post } from './request';
 export const addEvent = (event: EventType) => post('/api/v1/events', event);
 
 export const getEventById = (path: string) => get(path);
+
+export const getEvents = () => get('/api/v1/events');
