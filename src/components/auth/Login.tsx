7756da9e15d70b64041ed5716c96113863cd42cb
bwapes <benwaples@gmail.com>
2020-11-28 16:17:58 -0800

    login global state

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,19 +1,31 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import {
+  setUsernameAction,
+  setUserRoleAction,
+} from '../../actions/authActions';
 import { postLogin } from '../../services/authApi';
 
-export default function Login() {
+export default function Login(): JSX.Element {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const history = useHistory();
+  const dispatch = useDispatch();
 
-  const handleSubmit = async (event: { preventDefault: () => void }) => {
+  // eslint-disable-next-line consistent-return
+  async function handleSubmit(event: { preventDefault: () => void }) {
     event.preventDefault();
+
     const user = await postLogin(username, password);
 
     if (user.status === 500) return alert('incorrect username/password');
     history.push('/calendar');
-  };
+
+    dispatch(setUserRoleAction(user.userRole));
+    dispatch(setUsernameAction(user.username));
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -28,7 +40,7 @@ export default function Login() {
         value={password}
         onChange={({ target }) => setPassword(target.value)}
       />
-      <button>Login</button>
+      <button type="submit">Login</button>
     </form>
   );
 }
